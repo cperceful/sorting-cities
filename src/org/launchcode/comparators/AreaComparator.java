1200d594b23c854c4114aad0f320129ac184e103
cperceful <cpercef1@gmail.com>
2017-04-03 19:57:51 -0500

    Changed sorting to highest to lowest

--- src/org/launchcode/comparators/AreaComparator.java
+++ src/org/launchcode/comparators/AreaComparator.java
@@ -11,9 +11,9 @@ public class AreaComparator implements Comparator<City> {
 
     public int compare(City o1, City o2){
         if (o1.getArea() > o2.getArea()){
-            return 1;
-        } else if (o1.getArea() < o2.getArea()){
             return -1;
+        } else if (o1.getArea() < o2.getArea()){
+            return 1;
         } else {
             return 0;
         }
--- src/org/launchcode/comparators/PopulationComparator.java
+++ src/org/launchcode/comparators/PopulationComparator.java
@@ -13,9 +13,9 @@ public class PopulationComparator implements Comparator<City> {
     public int compare(City o1, City o2){
 
         if (o1.getPopulation() > o2.getPopulation()){
-            return 1;
-        } else if (o1.getPopulation() < o2.getPopulation()){
             return -1;
+        } else if (o1.getPopulation() < o2.getPopulation()){
+            return 1;
         } else {
             return 0;
         }
